3a19fcf780370d554f883fdbe85f61999295a492
Greg <mgrke1@yahoo.gr>
2021-11-11 22:09:54 +0200

    Add user registration and login authorization

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
 
 const mongoose = require('mongoose');
-const url = 'mongodb://127.0.0.1:27017/skgcode';
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const createMockData = require('./createMockData');
 const User = require('./models/user');
 
-mongoose.connect(url, { useNewUrlParser: true }, async () => {
-  console.log('Database connected: ', url);
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true }, async () => {
+  console.log('Database connected: ', process.env.DB_URL);
   // get all collections, just as an exercise
   // const collections = await mongoose.connection.db.listCollections().toArray();
   // console.log(collections);
@@ -31,8 +33,54 @@ app.get('/users/:id', async (req, res) => {
   res.send(userInfo);
 });
 
+app.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+
+  const user = await User.find({ username, password });
+  if (user) {
+    const user = await new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    user.save((err, user) => {
+      if (err) {
+        res.status(500).send({
+          message: err,
+        });
+        return;
+      } else {
+        res.status(200).send({
+          message: 'User Registered successfully',
+        });
+      }
+    });
+  } else {
+    return res.status(500).send({ message: 'User already exists.' });
+  }
+});
+
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+
+  const user = await User.find({ username, password });
+  if (user) {
+    const token = jwt.sign({ id: user.id }, process.env.API_SECRET, {
+      expiresIn: '1d',
+    });
+
+    res
+      .status(200)
+      .send({ user, message: 'Login Successful', accessToken: token });
+  } else {
+    return res
+      .status(404)
+      .send({ message: 'Username or password is invalid.' });
+  }
+});
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
-
-
