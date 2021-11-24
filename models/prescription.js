ab30a2cdbd674428e0c5ac1fd94420cd05949a79
LiaPet <liaptsispetros@gmail.com>
2021-11-24 20:58:00 +0200

    Co-authored-by: Greg Magkos <grigmag@users.noreply.github.com>

--- models/prescription.js
+++ models/prescription.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+// mongoose.Schema.ObjectId
+// mongoose.SchemaTypes.ObjectId
+// mongoose.Types.ObjectId
+
 const prescriptionSchema = new Schema({
-  userId: String,
+  userId: mongoose.SchemaTypes.ObjectId,
   title: String,
   description: String,
   date: Date,
