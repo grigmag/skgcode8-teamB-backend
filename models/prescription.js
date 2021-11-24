fbde57df207ee276879d878ae803e6612d0b990c
Greg <grmagkos@gmail.com>
2021-11-24 21:10:01 +0200

    Remove leftover comment

--- models/prescription.js
+++ models/prescription.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-// mongoose.Schema.ObjectId
-// mongoose.SchemaTypes.ObjectId
-// mongoose.Types.ObjectId
-
 const prescriptionSchema = new Schema({
   userId: mongoose.SchemaTypes.ObjectId,
   title: String,
